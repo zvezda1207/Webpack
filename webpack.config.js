2bfa1558a4c640ef149c32392f7912ea8b166cb8
Ira Tka4eva <ira.tka4eva2011@yandex.ru>
2024-12-14 16:12:48 +0300

    task is ready

--- webpack.config.js
+++ webpack.config.js
@@ -7,44 +7,26 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-      {
+      {       
         test: /\.css$/i,
         use: [MiniCSSExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      }
+      },     
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/index.html',
+       template: './src/index.html',
       filename: './index.html',
     }),
     new MiniCSSExtractPlugin(),
   ],
+  devServer: {
+    static: './dist',
+  }
 };
